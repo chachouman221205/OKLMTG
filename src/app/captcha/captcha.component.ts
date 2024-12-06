d2302f71a92db4986ac821d1597c0a41fe9c4b0d
chachouman221205 <boudekhani.nassim@gmail.com>
2024-12-06 05:07:13 +0100

    GameChat est insupportable

--- src/app/captcha/captcha.component.ts
+++ src/app/captcha/captcha.component.ts
@@ -9,32 +9,41 @@ import { CommonModule } from '@angular/common';
 })
 export class CaptchaComponent {
   @Output() messageEvent = new EventEmitter<string>();
-  goal = Math.floor(Math.random() * 50);
-  tries = 10;
-  thune = 0;
+  goal = Math.floor(Math.random() * (50-1)+1);
+  random = Math.random();
+  default_tries = 7;
+  tries = this.default_tries;
+  jetons = 0;
   result_message = "";
+  gained = 0;
+  max_gain = 5;
 
   add_money() {
-    this.thune += 1;
+    this.gained = Math.floor(Math.random()*(this.max_gain-1)+1);
+    this.jetons += this.gained;
+    if (this.random < 0.05) {
+      this.gained += 40;
+      this.jetons += 40;
+    }
   }
 
   reset_money() {
-    this.thune = 0;
+    this.jetons = 0;
   }
 
   validate() {
-    if (this.thune == this.goal) {
+    if (this.jetons == this.goal) {
       this.messageEvent.emit("captcha validated");
     }
     else
     {
-      if (this.thune > this.goal) {
-        this.result_message = "Vous avez obtenu trop d'argent";
+      if (this.jetons > this.goal) {
+        this.result_message = "Vous avez obtenu trop de jetons";
       }
-      if (this.thune < this.goal) {
-        this.result_message = "Vous n'avez pas obtenu assez d'argent";
+      if (this.jetons < this.goal) {
+        this.result_message = "Vous n'avez pas obtenu assez de jetons";
       }
-      if (this.tries < 1) {
+      if (this.tries <= 1) {
         this.messageEvent.emit("captcha failed");
       }
       this.tries -= 1;
