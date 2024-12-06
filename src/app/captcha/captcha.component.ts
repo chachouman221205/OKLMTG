a0d9d646874ac488a53073922a995a0da7acb44c
chachouman221205 <boudekhani.nassim@gmail.com>
2024-12-06 02:06:26 +0100

    Captcha is now a clicker-guessing game

--- src/app/captcha/captcha.component.ts
+++ src/app/captcha/captcha.component.ts
@@ -9,9 +9,10 @@ import { CommonModule } from '@angular/common';
 })
 export class CaptchaComponent {
   @Output() messageEvent = new EventEmitter<string>();
-  goal = 50;
-  tries = 3;
+  goal = Math.floor(Math.random() * 50);
+  tries = 10;
   thune = 0;
+  result_message = "";
 
   add_money() {
     this.thune += 1;
@@ -27,6 +28,12 @@ export class CaptchaComponent {
     }
     else
     {
+      if (this.thune > this.goal) {
+        this.result_message = "Vous avez obtenu trop d'argent";
+      }
+      if (this.thune < this.goal) {
+        this.result_message = "Vous n'avez pas obtenu assez d'argent";
+      }
       if (this.tries < 1) {
         this.messageEvent.emit("captcha failed");
       }
